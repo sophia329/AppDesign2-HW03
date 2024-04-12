7bcaf360640530b5b92b61d48f26c1f5aa748a47
sophia329 <sophiarajen329@gmail.com>
2024-04-12 16:15:16 -0500

    commit

---
+++ PetStore-master/PetStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PetStore.Domain.Abstract;
+
+namespace PetStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository myrepository;
+
+        public ProductController (IProductRepository productRepository)
+        {
+            this.myrepository = productRepository;
+        }
+        public ViewResult List()
+        {
+            return View(myrepository.Products);
+        }
+    }
+}
